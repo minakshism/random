2e4a59306c22b223ac1e8c4f0de0b90e12fa944f
minakshism <minakshi129@gmail.com>
2018-03-16 23:27:18 +0000

    sss

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -6,11 +6,6 @@ sap.ui.define([
 
 	return BaseController.extend("nl.controller.Home", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf nl.view.Home
-		 */
 			onInit: function() {
 				this.getView().setModel(this.getOwnerComponent().getModel("NorthWindDataModel"));
 			
